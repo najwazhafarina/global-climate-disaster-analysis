479844b4a6a3908d3fa6890e6e5ae90570253121
najwazhafarina <146604665+najwazhafarina@users.noreply.github.com>
2026-03-15 22:17:25 +0700

    Update and rename data_cleaning.py to python/data_cleaning.py

--- python/data_cleaning.py
+++ python/data_cleaning.py
@@ -1,17 +1,11 @@
 import pandas as pd
 
-# ==============================
-# 1. LOAD DATASET
-# ==============================
 
 df = pd.read_excel("data/raw/emdat_disasters.xlsx")
 
 print("Original dataset shape:")
 print(df.shape)
 
-# ==============================
-# 2. SELECT IMPORTANT COLUMNS
-# ==============================
 
 columns_needed = [
     "DisNo.",
@@ -35,28 +29,18 @@ df = df[columns_needed]
 print("\nAfter selecting columns:")
 print(df.shape)
 
-# ==============================
-# 3. HANDLE MISSING VALUES
-# ==============================
 
 df["Total Deaths"] = df["Total Deaths"].fillna(0)
 df["Total Affected"] = df["Total Affected"].fillna(0)
 df["Total Damage ('000 US$)"] = df["Total Damage ('000 US$)"].fillna(0)
 
-# drop rows yang tidak punya country atau year
 df = df.dropna(subset=["Country", "Start Year"])
 
-# ==============================
-# 4. DATA TYPE CLEANING
-# ==============================
 
 df["Start Year"] = df["Start Year"].astype(int)
 df["Total Deaths"] = df["Total Deaths"].astype(int)
 df["Total Affected"] = df["Total Affected"].astype(int)
 
-# ==============================
-# 5. RENAME COLUMNS (SQL FRIENDLY)
-# ==============================
 
 df = df.rename(columns={
     "DisNo.": "disaster_id",
@@ -75,9 +59,6 @@ df = df.rename(columns={
     "Longitude": "longitude"
 })
 
-# ==============================
-# 6. SAVE CLEAN DATASET
-# ==============================
 
 df.to_csv("data/cleaned/disasters_clean.csv", index=False)
 
